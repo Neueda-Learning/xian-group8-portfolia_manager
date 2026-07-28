f7723e7cbce853526474ff52bbbb5e2f337e711b
lito <1439711768@qq.com>
2026-07-28 03:15:44 +0000

    add HoldingRepository and HoldingService to manage portfolio holdings

---
+++ src/main/java/cn/lito/portfoilomanager/service/HoldingService.java
@@ -0,0 +1,35 @@
+package cn.lito.portfoilomanager.service;
+
+import cn.lito.portfoilomanager.model.Holding;
+import cn.lito.portfoilomanager.repository.HoldingRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class HoldingService {
+
+    private final HoldingRepository repository;
+
+    public HoldingService(HoldingRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Holding> getAllHoldings() {
+        return repository.findAll();
+    }
+
+    public Holding getHoldingById(int id) {
+        return repository.findById(id);
+    }
+
+    public Holding addHolding(Holding holding) {
+        int id = repository.save(holding);
+        holding.setId(id);
+        return holding;
+    }
+
+    public boolean deleteHolding(int id) {
+        return repository.deleteById(id) > 0;
+    }
+}
