a7fe92a8f6a8b8a759bddc21cf00bffb8e055408
lito <1439711768@qq.com>
2026-07-28 03:09:32 +0000

    initialize Portfolio Manager application with database schema and configuration

---
+++ src/main/java/cn/lito/portfoilomanager/PortfoiloManagerApplication.java
@@ -0,0 +1,13 @@
+package cn.lito.portfoilomanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfoiloManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfoiloManagerApplication.class, args);
+    }
+
+}
